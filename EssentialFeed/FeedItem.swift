45bc60aeacfff3d1b46fb84e4aaf7a5470c7b91c
Abdelrahman Mohamed <Obada.semary@gmail.com>
2021-06-23 21:50:52 +0300

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Abdelrahman Mohamed on 23.06.2021.
+//
+
+import Foundation
+
+struct FeedItem {
+
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
