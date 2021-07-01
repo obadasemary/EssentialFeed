d932cc7319ccdbd8b8d9511bb560eba4439efe3f
Abdelrahman Mohamed <Obada.semary@gmail.com>
2021-07-01 17:08:44 +0300

    Add more test assertions for different HTTP status code

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -18,11 +18,13 @@ public final class RemoteFeedLoader {
     private let client: HTTPClient
 
     public enum Error: Swift.Error {
+        
         case connectivity
         case invalidData
     }
 
     public init(url: URL = URL(string: "https://a-url.com")!, client: HTTPClient) {
+
         self.url = url
         self.client = client
     }
