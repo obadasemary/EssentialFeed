365690952d48cea9fdf66917e44de2076470b667
Abdelrahman Mohamed <Obada.semary@gmail.com>
2021-08-29 23:59:04 +0300

    Simplify type declarations with a helper function

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -42,7 +42,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 
         let (sut, client) = makeSUT()
 
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity)) {
+        expect(sut, toCompleteWith: failure(.connectivity)) {
             let clientError = NSError(domain: "Test", code: 0)
             client.complete(with: clientError)
         }
@@ -54,7 +54,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 
         let samples = [199, 201, 300, 400, 500]
         samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData)) {
+            expect(sut, toCompleteWith: failure(.invalidData)) {
                 let json = makeItemsJSON([])
                 client.complete(withStatusCode: code, data: json, at: index)
             }
@@ -65,7 +65,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 
         let (sut, client) = makeSUT()
 
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData)) {
+        expect(sut, toCompleteWith: failure(.invalidData)) {
             let invalidJSON = Data.init("invalid json".utf8)
             client.complete(withStatusCode: 200, data: invalidJSON)
         }
@@ -144,6 +144,10 @@ class RemoteFeedLoaderTests: XCTestCase {
         )
 
         return (sut, client)
+    }
+
+    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+        return .failure(error)
     }
 
     private func trackForMemoryLeaks(
