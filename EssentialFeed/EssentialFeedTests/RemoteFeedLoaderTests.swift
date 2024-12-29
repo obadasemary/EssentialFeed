33d007ed1a8087bda7c120fe44b9603d2f4aea1f
Abdelrahman Mohamed <obada.semary@gmail.com>
2024-12-30 01:49:59 +0300

    Delivers connectivity error on client error

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -35,6 +35,17 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSut()
+        client.error = NSError(domain: "Test", code: 0)
+        var capturedError: RemoteFeedLoader.Error?
+        sut.load { error in
+            capturedError = error
+        }
+        
+        XCTAssertEqual(capturedError, .connectivity)
+    }
+    
     // MARK: - Helpers
     
     private func makeSut(
@@ -46,9 +57,16 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var requestedURLs: [URL] = [URL]()
+        var requestedURLs = [URL]()
+        var error: Error?
         
-        func get(from url: URL) {
+        func get(
+            from url: URL,
+            completion: @escaping (Error) -> Void
+        ) {
+            if let error = error {
+                completion(error)
+            }
             requestedURLs.append(url)
         }
     }
