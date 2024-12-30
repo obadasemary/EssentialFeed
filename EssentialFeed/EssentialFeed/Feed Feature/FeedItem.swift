7f3b38132a3eefdf7f618a9ec52283d8450462cd
Abdelrahman Mohamed <obada.semary@gmail.com>
2024-12-30 17:10:48 +0300

    Delivers items array on 200 HTTP response with JSON Items

--- EssentialFeed/EssentialFeed/Feed Feature/FeedItem.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedItem.swift
@@ -5,8 +5,17 @@
 import Foundation
 
 public struct FeedItem: Equatable {
-	let id: UUID
-	let description: String?
-	let location: String?
-	let imageURL: URL
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
+
+extension FeedItem: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case description
+        case location
+        case imageURL = "image"
+    }
 }
