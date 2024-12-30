4b0dc3d76be6505f470c35571e086386c6fb3272
Abdelrahman Mohamed <obada.semary@gmail.com>
2024-12-30 17:44:11 +0300

    Move Decodable logic to a new private Item struct to decouple the Feed Feature module from API implementation details

--- EssentialFeed/EssentialFeed/Feed Feature/FeedItem.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedItem.swift
@@ -10,12 +10,3 @@ public struct FeedItem: Equatable {
     let location: String?
     let imageURL: URL
 }
-
-extension FeedItem: Decodable {
-    private enum CodingKeys: String, CodingKey {
-        case id
-        case description
-        case location
-        case imageURL = "image"
-    }
-}
