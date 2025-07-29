29f3041e9bde9bbb6fb513aa461c6eb6963232a9
Abdelrahman Mohamed <obada.semary@gmail.com>
2025-07-30 00:49:40 +0300

    typo

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -24,7 +24,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
     
     func test_getFromURL_performsGETRequestWithUrl() {
         let url = anyURL()
-        let exp = expectation(description: "Wait for request")
+        let exp = expectation(description: "Wait for request completion")
         
         URLProtocolStub.observeRequests { request in
             XCTAssertEqual(request.url, url)
